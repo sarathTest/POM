108dd41d83803fb7a42f9db9444869f9b0b3899b
Koushik <koushik350@gmail.com>
2018-08-10 10:31:54 +0530

    added new methods in SeMethod

--- PatternObjectModel/src/main/java/wdMethods/SeMethods.java
+++ PatternObjectModel/src/main/java/wdMethods/SeMethods.java
@@ -172,6 +172,16 @@ public class SeMethods extends Reporter implements WdMethods{
 		} 
 		return bReturn;
 	}
+	
+	public String getCurrentUrl() {		
+		String bReturn = "";
+		try {
+			bReturn =  driver.getCurrentUrl();
+		} catch (WebDriverException e) {
+			reportStep("Unknown Exception Occured While fetching Title", "FAIL");
+		} 
+		return bReturn;
+	}
 
 	public String getAttribute(WebElement ele, String attribute) {		
 		String bReturn = "";
